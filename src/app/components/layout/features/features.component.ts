717cdbd8ab90e270d3ea1c5cdc9fe15b5c025af8
Razorphish <david@maras.co>
2024-04-04 15:29:22 -0700

    added some layouts

--- src/app/components/layout/features/features.component.ts
+++ src/app/components/layout/features/features.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAws, faAmazon } from '@fortawesome/free-brands-svg-icons';
+import {faCloud} from '@fortawesome/free-solid-svg-icons'
 @Component({
   standalone: true,
   selector: 'ma-features',
   templateUrl: 'features.component.html',
-  styleUrls: ['features.component.css']
+  styleUrls: ['features.component.css'],
+  imports: [FontAwesomeModule]
 })
 export class FeaturesComponent implements OnInit {
+
+  faAws = faAws;
+  faWeb = faCloud;
+  faAmazon = faAmazon;
+
 
   constructor() { }
 
